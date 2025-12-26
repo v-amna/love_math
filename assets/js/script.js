1da7b4194de4b4f52e1448d9a501f9e3760344c9
v-amna <amnamampad@gmail.com>
2025-12-26 19:07:14 +0100

    add javascript for take two random dingit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,31 @@ document.addEventListener("DOMContentLoaded",function(){
             }
             else{
                 let gameType=this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                //alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
 
     }
-
+  runGame("addition");
 })
 
-function runGame(){
+/**
+ * The main game loop ,called when the script is first loaded
+ * and after the users answer has been processed
+ */
+function runGame(gameType){
+    //create random numbers between 1 and 25
+    let num1=Math.floor(Math.random()*25)+1;
+    let num2=Math.floor(Math.random()*25)+1;
+
+    if(gameType==="addition"){
+        displayAdditionQuestion(num1,num2);
+    }
+    else{
+        alert(`Unknown gametype :${gameType}`);
+        throw `Unknown gametype :${gameType}.Aborting!`;
+    }
 
 }
 function checkAnswer(){
@@ -34,7 +50,10 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="+";
 
 }
 function displaySubtractQuestion(){
