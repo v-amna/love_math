41cac1f829f74943a60a8a77afb3c6eb419b02e4
v-amna <amnamampad@gmail.com>
2025-12-26 20:14:50 +0100

    add code to count correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,9 +49,11 @@ function checkAnswer(){
     let isCorrect= userAnswer===calculatedAnswer[0];
     if(isCorrect){
         alert("hey you got it :D")
+        incrementScore();
     }
     else{
         alert(`Awww...you answerd${userAnswer}.The correct answer${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -73,10 +75,21 @@ function calculateCorrectAnswer(){
         throw `Unimpliment operator ${operator}.Aborting!`;
        }
 }
+/**
+ * gets the current score from the DOM and increment it by 1
+ */
 function incrementScore(){
+    let oldScore=parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText= ++oldScore;
 
 }
+/**
+ * gets the current tally of incorrect answer from the DOM and increment it by 1
+ */
 function incrementWrongAnswer(){
+     let oldScore=parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText= ++oldScore;
+
 
 }
 function displayAdditionQuestion(operand1, operand2){
