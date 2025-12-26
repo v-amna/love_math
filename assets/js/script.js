0d1eea263a85c753ef1ce2ce7e56f42d849c1988
v-amna <amnamampad@gmail.com>
2025-12-26 21:32:59 +0100

    add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,10 @@ function runGame(gameType){
         displaySubtractQuestion(num1,num2);
 
     }
+     else if(gameType==="division"){
+        displayDivisionQuestion(num1,num2);
+
+    }
     else{
         alert(`Unknown gametype :${gameType}`);
         throw `Unknown gametype :${gameType}. Aborting!`;
@@ -93,6 +97,10 @@ function calculateCorrectAnswer(){
          return[operand1-operand2,"subtract"];
 
        }
+       else if(operator==="/"){
+         return[operand1/operand2,"division"];
+
+       }
        else{
         alert(`Unimpliment operator ${operator}`);
         throw `Unimpliment operator ${operator}.Aborting!`;
@@ -134,6 +142,9 @@ function displayMultiplyQuestion(operand1, operand2){
 
 
 }
-function displayDivisionQuestion(){
-
+function displayDivisionQuestion(operand1, operand2){
+    operand1=operand1*operand2;
+    document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="/";
 }
