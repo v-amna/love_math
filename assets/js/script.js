1fa9a793b722b98575c8fb84c8a98400442561d5
v-amna <amnamampad@gmail.com>
2025-12-26 19:59:22 +0100

    add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,12 @@ document.addEventListener("DOMContentLoaded",function(){
        for(let button of buttons) {
         button.addEventListener("click", function(){
             if(this.getAttribute("data-type")==="submit"){
-                alert("You clicked Submit!");
+               
+                checkAnswer();
             }
             else{
                 let gameType=this.getAttribute("data-type");
-                //alert(`You clicked ${gameType}`);
+                
                 runGame(gameType);
             }
         })
@@ -34,15 +35,43 @@ function runGame(gameType){
     }
     else{
         alert(`Unknown gametype :${gameType}`);
-        throw `Unknown gametype :${gameType}.Aborting!`;
+        throw `Unknown gametype :${gameType}. Aborting!`;
     }
 
 }
+/**
+ * check the answer against the first element in
+ * the returned calculatecorrectAnswer array
+ */
 function checkAnswer(){
-
+    let userAnswer= parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer=calculateCorrectAnswer();
+    let isCorrect= userAnswer===calculatedAnswer[0];
+    if(isCorrect){
+        alert("hey you got it :D")
+    }
+    else{
+        alert(`Awww...you answerd${userAnswer}.The correct answer${calculatedAnswer[0]}!`);
+    }
+    runGame(calculatedAnswer[1]);
 }
+/**
+ * gets the operands (the numbers) and the operator (plus,minus etc)
+ * directly from the dom , and returns the correct answer.
+ */
 function calculateCorrectAnswer(){
+        let operand1=parseInt(document.getElementById('operand1').innerText);
+        let operand2=parseInt(document.getElementById('operand2').innerText);
+        let operator=document.getElementById("operator").innerText;incrementScore
+    if(operator==="+")
+        {
+             return[operand1+operand2,"addition"];
 
+       }
+       else{
+        alert(`Unimpliment operator ${operator}`);
+        throw `Unimpliment operator ${operator}.Aborting!`;
+       }
 }
 function incrementScore(){
 
