222a6890ec6573e459ed7ac4524bdb9b0dafbc82
v-amna <amnamampad@gmail.com>
2025-12-26 20:28:14 +0100

    add code to multiplication game

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,11 @@ function runGame(gameType){
 
     if(gameType==="addition"){
         displayAdditionQuestion(num1,num2);
+    }else if(gameType==="multiply"){
+        displayMultiplyQuestion(num1,num2);
+
     }
+    
     else{
         alert(`Unknown gametype :${gameType}`);
         throw `Unknown gametype :${gameType}. Aborting!`;
@@ -70,6 +74,10 @@ function calculateCorrectAnswer(){
              return[operand1+operand2,"addition"];
 
        }
+       else if(operator==="x"){
+         return[operand1*operand2,"multiply"];
+
+       }
        else{
         alert(`Unimpliment operator ${operator}`);
         throw `Unimpliment operator ${operator}.Aborting!`;
@@ -99,9 +107,16 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 function displaySubtractQuestion(){
+     document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="-";
 
 }
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="x";
+
 
 }
 function displayDivisionQuestion(){
